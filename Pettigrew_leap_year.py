2389eb4f766b3bfb0eacd5e61288d91f1ea1e0cf
Tyler Pettigrew <pettigrt@oregonstate.edu>
2021-01-25 16:28:38 -0800

    Updated untested leap year

--- Pettigrew_leap_year.py
+++ Pettigrew_leap_year.py
@@ -7,6 +7,8 @@
 """
 
 
+
+
 """leap_year
     Inputs:
         year: a positive integer number
@@ -14,7 +16,6 @@
         If the given year is a leap year based on the given criteria, print that the year is a leap year.
         Otherwise, say it isn't
 """
-
 
 def leap_year(year):
     if year % 4 == 0:
@@ -32,17 +33,7 @@ def leap_year(year):
 if __name__ == '__main__':
     year = input("Enter the year, or 'q' to quit: ")
 
-    # If the escape character 'q' is pressed, exit the program
-    if year.strip().lower() == "q":
-        exit(0)
-    # Strip any extra spaces, and try to convert the string to a number. If it fails, reprompt until a correct number
-    # is given or the user quits
-    year = year.strip()
-    while not (year.isnumeric()):
-        year = input("Year not recognized: Enter the year or q to quit: ")
+    #Run the program without any checking the input
 
-        if year.strip().lower() == "q":
-            exit(0)
-
-    year = abs(int(year.strip()))
+    year = int(year)
     leap_year(year)
